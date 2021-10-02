9324681be1b71867ff3d4fd22136e10795e8867f
srinjoy0810 <srinjoy2468@gmail.com>
2021-10-03 01:32:57 +0530

    final changes

--- springapp/src/main/java/com/examly/springapp/SecurityConfiguration.java
+++ springapp/src/main/java/com/examly/springapp/SecurityConfiguration.java
@@ -2,15 +2,18 @@ package com.examly.springapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -19,28 +22,42 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     UserDetailsService userDetailsService;
     @Autowired
 	AuthenticationSuccessHandler successHandler;
+    @Autowired
+	private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
     @Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+    @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/signup");
-        //web.ignoring().antMatchers("/login");
+        web.ignoring().antMatchers("/login");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+        		.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/hello").hasRole("USER")
+                .antMatchers("/hello").hasAnyRole("USER")
                 .antMatchers("/home").hasRole("USER")
                 .antMatchers("/image/**").hasRole("USER")
                 .antMatchers("/comment/**").hasRole("USER")
+                .antMatchers("/").permitAll()
                 .and().formLogin()
-				.successHandler(successHandler)
-				.and().logout();
+                .and()
+				.exceptionHandling().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+				//.successHandler(successHandler)
+				
     }
 
     @Bean
--- springapp/src/main/java/com/examly/springapp/controllers/loginCon.java
+++ springapp/src/main/java/com/examly/springapp/controllers/loginCon.java
@@ -1,17 +1,50 @@
 package com.examly.springapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import com.examly.springapp.MyUserDetailsService;
+import com.examly.springapp.models.AuthenticationResponse;
+import com.examly.springapp.models.JwtUtil;
 import com.examly.springapp.models.login;
+import com.examly.springapp.models.user;
 import com.examly.springapp.services.loginServ;
 @RestController
 public class loginCon {
 	public login login;
 	@Autowired
 	public loginServ ls;
-	@RequestMapping(method=RequestMethod.POST,value="/login")
-	boolean checkUser(@RequestBody login login) {
-		return ls.checkUser(login);
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody user user) throws Exception {
+
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword())
+			);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(user.getEmail());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
 	}
+
 }
+
