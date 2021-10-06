6e180a55c2a518002aa096a7d05899861427d4f6
Yashasvi-Berbalaje <yberbalaje404@gmail.com>
2021-10-06 06:03:29 +0000

    Updated pom.xml file

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,9 +2,8 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringappApplication {
 
 	public static void main(String[] args) {
