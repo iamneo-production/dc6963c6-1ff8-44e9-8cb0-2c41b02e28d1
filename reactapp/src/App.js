818b2d1d14913fc4b9613ecbf6fe5896f69bea02
Yashasvi-Berbalaje <yberbalaje404@gmail.com>
2021-10-14 05:03:45 +0000

    merged

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,6 +5,8 @@ import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './Pages/profile';
+import AdminHome from './admin/home';
+
 
 function App() {
   return (
@@ -25,6 +27,7 @@ function App() {
             <Route path="/signup" exact component={SignUp}></Route>
             <Route path="/profile" exact component={Profile}></Route>
             <Route path="/signin" exact component={SignIn}></Route>
+            <Route path="/admin" exact component={AdminHome}></Route>
           </Switch>
         </Router>
   );
