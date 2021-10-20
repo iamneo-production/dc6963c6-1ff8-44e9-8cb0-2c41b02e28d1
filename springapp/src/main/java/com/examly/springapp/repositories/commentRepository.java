e0e0e6716d6eebe4fd9a53ef3686e74fa502eb39
ShubhamRajanSingh <singhshubham87033@gmail.com>
2021-10-20 05:44:34 +0000

    Delted

--- springapp/src/main/java/com/examly/springapp/repositories/commentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.examly.springapp.models.Comment;
-
-public interface commentRepository extends JpaRepository<Comment,String>{
-
-}
--- springapp/src/main/java/com/examly/springapp/repositories/loginRepo.java
+++
@@ -1,8 +0,0 @@
-package com.examly.springapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.examly.springapp.models.login;
-
-public interface loginRepo extends CrudRepository<login, String>  {
-    public login deleteByEmail(String email);
-}
